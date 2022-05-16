582cbac0263ff4384445bca2ae2c4276b4a453a5
RobStafford <75902183+RobStafford@users.noreply.github.com>
2022-05-16 14:32:05 -0700

    Create reverseLinkedList.js

---
+++ reverseLinkedList.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    //several ways to deal with, but the head has no 'previous' node
+    let previous = null;
+    //start at the beginning
+    let current = head;
+    //do while you haven't finished moving through list
+    while (current) {
+        //set next to next of current 9to store as original ref is disappearing)
+        const next = current.next;
+        //reverse pointer
+        current.next = previous;
+        //now that we've changed pointer, call the one we just changed previous
+        previous = current;
+        //we saved next so we can find the old next, start over
+        current = next;        
+    }
+    return previous;
+    
+};
